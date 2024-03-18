616d7f74aa470b2d4bd8d335d2b1aa42feea60ab
gwanghyeongim <harryghgim@eoding.com>
2024-03-18 17:03:42 +0900

    마이그레이션 with lazy

--- product/models.py
+++ product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class Car(models.Model):
+    class Type(models.IntegerChoices):
+        SLOW = 1, _("느림")
+        MEDIUM = 2, _("중간")
+        FAST = 3, _("빠름")
+
+    name = models.CharField(verbose_name=_("이름"), max_length=100)
+    type = models.IntegerField(choices=Type.choices)
